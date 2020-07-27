29f59b071efb130b14923ac6b8b019039925e725
Mateus Calza <mateushenrique17@gmail.com>
2020-07-27 14:02:15 -0300

    TF using GPU

--- 1.js
+++ 1.js
@@ -1,3 +1,5 @@
+// Image source: javatpoint apud https://papers.bastolasushan.com/what-are-tensors/
+
 const tf = require('@tensorflow/tfjs-node')
 
 const a = tf.tensor(2)
---
+++ 4-tf-gpu.js
@@ -0,0 +1,13 @@
+const tf = require('@tensorflow/tfjs-node-gpu')
+
+const generateArray = size => Array.from(Array(size)).map((value, index) => index)
+
+const numbers = generateArray(10000000)
+
+const start = +new Date()
+const numbersTensor = tf.tensor(numbers)
+const sum = numbersTensor.sum()
+const c = numbersTensor.div(sum).sqrt()
+
+c.print()
+console.log((+new Date() - start) / 1000)
